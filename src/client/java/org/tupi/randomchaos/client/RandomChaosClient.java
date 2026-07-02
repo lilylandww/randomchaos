85b36b6b94fae34cef854ddb1e0991ba6fe0a4ee
Tupi <tupi@kpdev.asplythso.duckdns.org>
2026-07-02 18:15:35 +0800

    fix: register ChaosStatePayload on client so HUD receives state updates

--- src/client/java/org/tupi/randomchaos/client/RandomChaosClient.java
+++ src/client/java/org/tupi/randomchaos/client/RandomChaosClient.java
@@ -1,12 +1,15 @@
 package org.tupi.randomchaos.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import org.tupi.randomchaos.client.net.ClientNetworking;
 import org.tupi.randomchaos.client.hud.ChaosHudOverlay;
+import org.tupi.randomchaos.net.ChaosStatePayload;
 
 public class RandomChaosClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		PayloadTypeRegistry.clientboundPlay().register(ChaosStatePayload.TYPE, ChaosStatePayload.STREAM_CODEC);
 		ClientNetworking.register();
 		ChaosHudOverlay.register();
 	}
